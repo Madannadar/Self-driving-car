647f0efb589272747b91a29a3586f3e3b9d42398
Madan <madanak0509@gmail.com>
2026-01-01 22:44:14 +0530

    continue: 134 Enhance sensor and car functionality to detect traffic; update controls and intersection logic

--- Sensor.js
+++ Sensor.js
@@ -9,17 +9,17 @@ class Sensor{
         this.readings=[];
     }
 
-    update(roadBorders){
+    update(roadBorders, traffic){
         this.#castRays();
         this.readings=[];
         for(let i=0;i<this.rays.length;i++){
             this.readings.push(
-                this.#getReading(this.rays[i],roadBorders)
+                this.#getReading(this.rays[i],roadBorders, traffic)
             );
         }
     }
 
-    #getReading(ray,roadBorders){
+    #getReading(ray,roadBorders, traffic){
         let touches=[];
 
         for(let i=0;i<roadBorders.length;i++){
@@ -32,6 +32,22 @@ class Sensor{
             if(touch){
                 touches.push(touch);
             }
+        }
+
+        for(let i = 0; i < traffic.length; i++){
+            const poly = traffic[i].polygon;
+            for(let j = 0; j < poly.length; j++){
+                const value = getIntersection(
+                    ray[0],
+                    ray[1],
+                    poly[j],
+                    poly[(j+1)%poly.length]
+                );
+                if(value){
+                    touches.push(value);
+                }
+            }
+            
         }
 
         if(touches.length==0){
@@ -51,7 +67,6 @@ class Sensor{
                 -this.raySpread/2,
                 this.rayCount==1?0.5:i/(this.rayCount-1)
             )+this.car.angle;
-
             const start={x:this.car.x, y:this.car.y};
             const end={
                 x:this.car.x-
--- controls.js
+++ controls.js
@@ -1,11 +1,18 @@
 class Controls{
-    constructor(){
+    constructor(type){
         this.forward = false;
         this.left = false;
         this.right = false;
         this.reverse = false;
-
-        this.#addKeyboardListeners(); // # means this is private no other file can access this function
+        switch(type){
+            case "KEYS":
+                this.#addKeyboardListeners();
+                break;
+            case "DUMMY":
+                this.forward = true;
+                break;
+        }
+         // # means this is private no other file can access this function
     } 
 
     #addKeyboardListeners(){
